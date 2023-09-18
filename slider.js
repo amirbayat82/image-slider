dfad6652a29c87ba1b2313af619c52c42f0181b0
Amir Bayat <125858363+amirbayat82@users.noreply.github.com>
2023-09-18 13:18:55 +0330

    Add comment

--- slider.js
+++ slider.js
@@ -1,11 +1,16 @@
+// Put all images sources in array
 let images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"];
+// set A counter for array index for next and prev btn 
 let imageIndex = 0;
 
+// select Next And prev button on HTML Document file
 let prev_btn = document.querySelector("#prev");
 let next_btn = document.querySelector("#next");
 
+// Select Image Tag In HTML File
 let image_holder = document.querySelector("#img-holder");
 
+// Next image function for Next Image button On Slider
 function nextImg() {
   imageIndex++;
   if (imageIndex > images.length - 1) {
@@ -14,7 +19,7 @@ function nextImg() {
   image_holder.setAttribute("src", images[imageIndex]);
   console.log(imageIndex);
 }
-
+// Prev image function for prev Image button On Slider
 function prevImg() {
   imageIndex--;
   if (imageIndex < 0) {
@@ -24,8 +29,10 @@ function prevImg() {
   console.log(imageIndex);
 }
 
+// set interval for skip automaticaly images 
 setInterval(() => {
   nextImg();
 }, 3000);
+// set event for next & prev buttons when clicked !
 next_btn.addEventListener("click", nextImg);
 prev_btn.addEventListener("click", prevImg);
